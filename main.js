1e3dd6ec189ef279f55d2ec68e0d635deffbd522
Wodd-Off <offwodd@gmail.com>
2023-06-21 15:35:10 +0200

    Pushing version 0.0.1 ( not finished )

---
+++ main.js
@@ -0,0 +1,54 @@
+const { app, BrowserWindow, ipcMain } = require('electron')
+const path = require('path')
+
+let mainFrameWindows;
+
+function createWindow () {
+  mainFrameWindows = new BrowserWindow
+  ({
+    width: 1280,
+    height: 720,
+    title: "Base Launcher Minecraft",
+    webPreferences: {
+      nodeIntegration: true,
+      webviewTag: true,
+      preload: path.join(__dirname, 'src/renderer.js')
+    },
+  roundedCorners: true,
+  frame: false,
+  })
+
+  mainFrameWindows.loadFile('src/index.html')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (mainFrameWindows.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+ipcMain.on('close-window', () => {
+    app.quit();
+});
+  
+ipcMain.on('minimize-window', () => {
+    mainFrameWindows.minimize();
+});
+  
+ipcMain.on('maximize-window', () => {
+    if (mainFrameWindows.isMaximized()) {
+      mainFrameWindows.unmaximize();
+    } else {
+      mainFrameWindows.maximize();
+    }
+});
